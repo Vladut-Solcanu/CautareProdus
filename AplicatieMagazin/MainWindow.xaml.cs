71244ed5a37fd77fe5e802564edc724c8dfcc5c8
Vladut-Solcanu <vladut.solcanu@student.usv.ro>
2026-04-21 18:10:48 +0300

    WPF

---
+++ AplicatieMagazin/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using ModeleMagazin;
+using LogicaMagazine;
+
+namespace AplicatieMagazin
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IStocareProduse admin = new AdministrareProduseFisier("Produse.txt");
+
+            Produs produsNou = new Produs()
+            {
+                Id = 999, 
+                MagazinId = 1,
+                Nume = "Ciocolată cu Lapte",
+                
+                Culoar = 3,
+                Raft = 5
+            };
+
+            
+            admin.AddProdus(produsNou);
+
+            
+            MessageBox.Show("Produsul a fost salvat cu succes în fișier!");
+
+           
+            lblId.Content = "ID Produs: " + produsNou.Id;
+            lblNume.Content = "Nume: " + produsNou.Nume;
+            lblCategorie.Content = "Locație: Culoarul " + produsNou.Culoar + ", Raftul " + produsNou.Raft;
+        }
+    }
+}
